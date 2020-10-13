ed4b51eab8ce0b423146aab4e53fd4d2377a9aea
Garrett Smiley <garrett@soraschools.com>
2020-10-12 23:04:14 -0400

    first commit

---
+++ client/README.md
@@ -0,0 +1,32 @@
+## Welcome to the Sora Full-Stack Coding Challenge!
+
+If you wish to work at Sora, follow the instructions below!
+
+### Challenge Description
+
+At Sora, each of our students have skills that are rated on a scale from level 0-4 based on their demonstrated knowledge. This is our replacement for "grades". In this scenario, a student has a question about a specific skill. So, create a web app using React and Express that helps the student find the correct person to answer their question about a selected skill.
+
+Selection logic: A question should go to a student who is a level 3 or 4 in the skill. If there are two or more students who meet those level requirements, prioritize students in the same house then prioritize higher level, in that order. However, ensure the selected student is always at least one level higher than the student. If no student is a level 3 or 4 and is not at least one level higher, it will be directed to the Faculty member who has a tag for the skill's subject.
+
+### Example Outputs
+
+_assuming you're the 'Tester' student_
+
+- skill_id === 1 => Faculty member 'Carolyn Reeves'
+- skill_id === 3 => Student 'Ludwig Wittgenstein'
+- skill_id === 4 => Faculty member 'Michael Granado'
+- skill_id === 6 => Student 'Max Stirner'
+
+### Judging Criteria
+
+We'll be examining the app's logic as well as its design and user experience. We expect the app to be reactive to different screen sizes. Imagine that your small app is going to production tommorrow! Although it's outside of the scope of this challenge, expect further questions about potential database desing.
+
+### Getting Started
+
+shell npm install
+To run react app:
+shell cd client & npm start
+To run express server:
+shell node server.js
+
+Feel free to use or scaffold code or completely disregard it! It's only there to make your life easier. A good place to start would be the 'server.js' file.
---
+++ server.js
@@ -0,0 +1,126 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+
+// console.log that your server is up and running
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+// All skills not explicitly referenced in the "skills" property can be
+// assumed to be 0.
+app.get("/students", (req, res) => {
+  const students = [
+    {
+      id: 1,
+      name: "Tester",
+      email: "test@soraschools.com",
+      house: "Heqet",
+      skills: [
+        { id: 3, level: 2 },
+        { id: 4, level: 3 },
+        { id: 6, level: 1 },
+      ],
+    },
+    {
+      id: 2,
+      name: "Ludwig Wittgenstein",
+      email: "inaflyjar@soraschools.com",
+      house: "Heqet",
+      skills: [
+        { id: 3, level: 4 },
+        { id: 4, level: 1 },
+      ],
+    },
+    {
+      id: 3,
+      name: "Max Stirner",
+      email: "nothingtome@soraschools.com",
+      house: "Heqet",
+      skills: [
+        { id: 1, level: 2 },
+        { id: 3, level: 3 },
+        { id: 6, level: 3 },
+      ],
+    },
+    {
+      id: 4,
+      name: "Simone de Beauvoir",
+      email: "strongnproud@soraschools.com",
+      house: "Nightshade",
+      skills: [
+        { id: 4, level: 3 },
+        { id: 5, level: 3 },
+        { id: 6, level: 4 },
+      ],
+    },
+    {
+      id: 2,
+      name: "Friedrich Nietzsche",
+      email: "iamtheubermench@soraschools.com",
+      house: "Nightshade",
+      skills: [
+        { id: 3, level: 4 },
+        { id: 4, level: 3 },
+        { id: 4, level: 1 },
+        { id: 6, level: 1 },
+      ],
+    },
+  ];
+  res.send({ result: students });
+});
+
+app.get("/current-user-id", (req, res) => {
+  const currentUserId = 1; // don't assume this will always be true
+  res.send({ result: currentUserId });
+});
+
+app.get("/faculty", (req, res) => {
+  const faculty = [
+    {
+      id: 1,
+      name: "Michael Granado",
+      subject_ids: [3, 4],
+    },
+    {
+      id: 2,
+      name: "Carolyn Reeves",
+      subject_ids: [1, 2],
+    },
+  ];
+  res.send({ result: faculty });
+});
+
+app.get("/skills", (req, res) => {
+  const skills = [
+    {
+      id: 1,
+      title: "Algebra Skill",
+      subject_id: 1,
+    },
+    {
+      id: 2,
+      title: "Geometry skill",
+      subject_id: 1,
+    },
+    {
+      id: 3,
+      title: "Biology Skill",
+      subject_id: 2,
+    },
+    {
+      id: 4,
+      title: "Physics Skill",
+      subject_id: 2,
+    },
+    {
+      id: 5,
+      title: "Philosophy Skill",
+      subject_id: 3,
+    },
+    {
+      id: 6,
+      title: "American History Skill",
+      subject_id: 4,
+    },
+  ];
+  res.send({ result: skills });
+});
